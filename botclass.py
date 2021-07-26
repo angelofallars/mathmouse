2c2130c35d3d6fb84ed31cb69864943030c36389
Angelo F <ba.fallaria@gmail.com>
2021-07-26 17:16:55 +0800

    Change formatting of messages with `````

--- botclass.py
+++ botclass.py
@@ -31,9 +31,9 @@ class QuizBot(discord.Client):
             score = 0
 
             # Send the pre-quiz messages for the user
-            await chnl.send("Starting quiz for {}..."
+            await chnl.send("``Starting quiz for {}...``"
                             .format(message.author))
-            await chnl.send("You have {} seconds each to answer {} question(s)!"
+            await chnl.send("``You have {} seconds each to answer {} question(s)!``"
                             .format(timeout, question_count))
 
             # Iterate through all of the questions
@@ -44,45 +44,45 @@ class QuizBot(discord.Client):
                 b = random.randint(1, 20)
                 answer = a + b
 
-                await chnl.send('**{}.** What is {} + {}?'.format(i, a, b))
+                await chnl.send('``**{}.** What is {} + {}?``'.format(i, a, b))
 
                 def is_correct(m):
                     return m.author == message.author and m.content.isdigit()
 
                 # Wait for the answer
                 try:
-                    guess = await self.wait_for('message',
+                    guess = await self.wait_for('``message``',
                                                 check=is_correct,
                                                 timeout=timeout)
                 # Timeout
                 except asyncio.TimeoutError:
-                    await chnl.send('Sorry, you took too long it was {}.'
+                    await chnl.send('``Sorry, you took too long it was {}.``'
                                     .format(answer))
                     continue
 
                 # Correct Answer
                 if int(guess.content) == answer:
-                    await chnl.send('🎉You are right🎉!')
+                    await chnl.send('``🎉You are right🎉!``')
                     score += 1
                 # Wrong Answer
                 else:
-                    await chnl.send('Oops. The answer is {}.'
+                    await chnl.send('``Oops. The answer is {}.``'
                                     .format(answer))
 
             # Send the user's score
-            await chnl.send("Your score was **{}**/**{}**, {}!"
+            await chnl.send("``Your score was **{}**/**{}**, {}!``"
                             .format(score, question_count,
                                     message.author))
 
             # Send a message for perfect scores
             if score >= question_count:
                 await chnl.send
-                ("You answered all questions perfectly! Great job! 😍")
+                ("``You answered all questions perfectly! Great job! 😍``")
 
             # Send a message for perfect scores
             if score == 0:
                 await chnl.send
-                ("Better luck next time!")
+                ("``Better luck next time!``")
 
             # Update user's score in scores.csv
             csv_funcs.update_score(str(message.author.id), score)
@@ -99,6 +99,6 @@ class QuizBot(discord.Client):
 
                 for row in csv_reader:
                     if row[0] == user_id:
-                        await chnl.send("<@{}>'s score is {}."
+                        await chnl.send("``<@{}>'s score is {}.``"
                                         .format(row[0], row[1]))
                         break
